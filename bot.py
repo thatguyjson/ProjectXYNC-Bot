30ddb925bfb6f8645df709dda03badc1de61f334
Json <jasonarodriguez16@gmail.com>
2025-08-07 22:41:14 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,66 @@
-testing
+'''
+Project: XYNC Bot is a discord bot created using the Nextcord API Wrapper.
+This was created by Jason, also known as @Json or @drip___ on discord!
+For any questions, feel free to add and dm me on discord "drip___"
+'''
+import nextcord
+import mysql.connector
+import asyncio
+import time
+import random
+import requests
+import json
+import re
+import pytz
+import os
+from nextcord.ext import commands, tasks
+from nextcord.ui import View, Select
+from datetime import datetime, timedelta, timezone
+from constants import *
+from emojis import *
+
+"""
+these intents allow the bot to track member activity, read messages, monitor reactions,
+receive server-level events, and listen to messages in guilds. All these are necessary for bot functionality
+like moderation, command handling, and reacting to user input.
+"""
+intents = nextcord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.reactions = True
+intents.guilds = True
+intents.guild_messages = True
+intents.messages = True
+
+'''
+commands.bot(command_prefix='', intents=intents), we are setting the prefix
+that commands will run in discord itself. intents=intents is telling the bot
+to use the intents we previously set to True above so that we can use it and
+make the bot more versatile.
+'''
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+
+# This might be used somewhere maybe...
+msg_ids = {}
+
+# DB Connection below grabs all DB info related stuff in order to connect from Constants(Available in PebbleHost)
+db = mysql.connector.connect(
+    host=DBhost,
+    user=DBuser,
+    password=DBpassword,
+    database=DBdatabase,
+    port=DBport
+)
+
+"""
+down below is on_ready + bot.run
+"""
+@bot.event
+async def on_ready():
+    global welcomeChannel
+    welcomeChannel = bot.get_channel(1402518002552803378)
+    if welcomeChannel is None:
+        await log_to_channel("Could not find the welcome channel.")
+    else:
+        await log_to_channel(f'Logged in as {bot.user.name}. Now commencing all startup processes.')
